b96229c10b230ac3f9f9e2bf73aad2f3faff1ed6
yaga001 <babayagaisok@gmail.com>
2025-05-19 21:25:54 +0500

    Update page.tsx

--- app/seats/page.tsx
+++ app/seats/page.tsx
@@ -11,7 +11,6 @@ import { getCurrentUser, signOutUser } from "@/lib/auth"
 import type { UserType } from "@/lib/types"
 import MainLayout from "@/components/main-layout"
 
-
 export default function SeatsPage() {
   const searchParams = useSearchParams()
   const router = useRouter()
